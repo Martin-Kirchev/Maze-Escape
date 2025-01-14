b15c4f56a0ce3297ee79518cb1b30dd2b02f7e12
Martin K. <martin88.2016@abv.bg>
2025-01-14 18:51:46 +0200

    Add Login/Register funtion

--- Maze-Escape/Maze-Escape.cpp
+++ Maze-Escape/Maze-Escape.cpp
@@ -9,40 +9,102 @@
 
 using namespace std;
 
-boolean playerExist(string name) {
+void printNewLine() {
 
-	string directory = "D:\\Workspace\\Maze-Escape\\Maze-Escape\\Players\\" + name + ".txt";
+	cout << endl;
+}
+
+int getRandomNumber(int max) {
+
+	srand(time(0));
+
+	return (rand() % max) + 1;
+}
+
+string toUpper(string word) {
+
+	string upperWord;
+	char symbols[10];
+
+	strcpy_s(symbols, word.c_str());
+
+	int iterator = 0;
+
+	while (symbols[iterator] != NULL) {
+
+		upperWord += toupper(symbols[iterator++]);
+	}
+
+	return upperWord;
+}
+
+boolean nameValidator(string playerName) {
+
+	int nameLength = playerName.length();
+
+	return (nameLength >= 1) && (nameLength <= 50);
+}
+
+boolean playerExist(string playerName) {
+
+	string directory = "D:\\Workspace\\Maze-Escape\\Maze-Escape\\Players\\" + playerName + ".txt";
 
 	fstream playerFile(directory);
 
 	return playerFile.is_open();
 }
 
-void createPlayerFile(string name) {
+void createPlayerFile(string playerName) {
 
-	while (playerExist(name)) {
-
-		cout << "Player \"" <<  name  << "\" already exists!" << endl;
-		cin >> name;
-	}
-
-	ofstream playerFile("D:\\Workspace\\Maze-Escape\\Maze-Escape\\Players\\" + name + ".txt");
+	ofstream playerFile("D:\\Workspace\\Maze-Escape\\Maze-Escape\\Players\\" + playerName + ".txt");
 	playerFile.close();
 }
 
-void createPlayerData(string name) {
+void createPlayerData(string playerName) {
 
-	fstream playerFile("D:\\Workspace\\Maze-Escape\\Maze-Escape\\Players\\" + name + ".txt", ios::app);
+	fstream playerFile("D:\\Workspace\\Maze-Escape\\Maze-Escape\\Players\\" + playerName + ".txt", ios::app);
 
 	playerFile << "Lives: 0" << endl;
 	playerFile << "Coins: 0" << endl;
-	playerFile << "Maps:" << endl;
+	playerFile << "Completed Maps:" << endl;
+	playerFile << "Unfinished Map:" << endl;
 }
 
-void createPlayer(string name) {
+void registerPlayer(string playerName) {
 
-	createPlayerFile(name);
-	createPlayerData(name);
+	while (!nameValidator(playerName)) {
+
+		cout << "Player name \"" << playerName << "\" must have between [1, 50] symbols!" << endl;
+		cin >> playerName;
+	}
+
+	while (playerExist(playerName)) {
+
+		cout << "Player \"" << playerName << "\" already exists!" << endl;
+		cin >> playerName;
+	}
+
+	createPlayerFile(playerName);
+	createPlayerData(playerName);
+
+	cout << "Register successful!" << endl;
+}
+
+void loginPlayer(string playerName) {
+
+	while (!nameValidator(playerName)) {
+
+		cout << "Player name \"" << playerName << "\" must have between [1, 50] symbols!" << endl;
+		cin >> playerName;
+	}
+
+	while (playerExist(playerName)) {
+
+		cout << "Player \"" << playerName << "\" doesn't exists!" << endl;
+		cin >> playerName;
+	}
+
+	cout << "Login successful!" << endl;
 }
 
 
@@ -52,19 +114,11 @@ void printMaze(int rows, int columns, char maze[20][20]) {
 	{
 		for (size_t colm = 0; colm < columns; colm++)
 		{
-
 			cout << maze[row][colm];
 		}
 
 		cout << endl;
 	}
-}
-
-int getRandomNumber(int max) {
-
-	srand(time(0));
-
-	return (rand() % max) + 1;
 }
 
 string getMapDirectory(string difficulty, int number, string size) {
@@ -93,30 +147,191 @@ void copyMapToArray(fstream& mazeFile, char maze[20][20]) {
 void createMap(string difficulty, int randomNumber, string size, char maze[20][20]) {
 
 	fstream mazeFile(getMapDirectory(difficulty, randomNumber, size), ios::in);
-
-	if (!mazeFile.is_open()) {
-
-		cout << "IDK";
-
-		mazeFile.close();
-		return;
-	}
-
 	copyMapToArray(mazeFile, maze);
 
 	mazeFile.close();
 }
 
-int main()
-{
+string* loadPlayerMaps(string playerName) {
+
+	string* mapArray = new string[10];
+
+	fstream playerFile("D:\\Workspace\\Maze-Escape\\Maze-Escape\\Players\\" + playerName + ".txt", ios::in);
+	string line;
+	boolean validLine = false;
+
+	int iterator = 0;
+
+	while (getline(playerFile, line)) {
+
+
+		if (line == "Unfinished Map:") {
+
+			return mapArray;
+		}
+		else if (line == "Completed Maps:") {
+
+			validLine = true;
+			continue;
+		}
+		else if (validLine) {
+
+			mapArray[iterator++] = line;
+		}
+	}
+
+	return mapArray;
+}
+
+boolean playerSystemCommandValidator(string command) {
+
+	return (command == "LOGIN") || (command == "REGISTER");
+}
+
+string startPlayerSystem() {
 
 	cout << "Login/Register" << endl;
 
 	string command;
-
 	cin >> command;
+	command = toUpper(command);
 
-	createPlayer(command);
+	while (!playerSystemCommandValidator(command)) {
+
+		cout << "Command \"" << command << "\" is invalid!" << endl;
+		cin >> command;
+		command = toUpper(command);
+	}
+
+	string playerName;
+	cin >> playerName;
+
+	if ((command == "LOGIN")) {
+
+		loginPlayer(playerName);
+	}
+	else {
+
+		registerPlayer(playerName);
+	}
+
+	return playerName;
+}
+
+boolean levelSelectorCommandValidator(string command) {
+
+	return (command == "CONTINUE") || (command == "SELECT");
+}
+
+void printListOfMaps(string* mapListPointer) {
+
+	int iterator = 1;
+
+	while (*mapListPointer != "") {
+
+		cout << iterator++ << ". " << *mapListPointer << endl;
+		mapListPointer++;
+	}
+}
+
+string getMapName(int mapNumber, string* mapListPointer) {
+
+	int iterator = 1;
+
+	while (*mapListPointer != "") {
+
+		if (iterator == mapNumber) {
+
+			return *mapListPointer;
+		}
+
+		iterator++;
+	}
+
+	return "WRONG_NUMBER_INPUT";
+}
+
+string getLastPlayedPlayerMap(string playerName) {
+
+	fstream playerFile("D:\\Workspace\\Maze-Escape\\Maze-Escape\\Players\\" + playerName + ".txt", ios::in);
+	string line;
+
+	while (getline(playerFile, line)) {
+
+		if (line == "Unfinished Map:") {
+
+			getline(playerFile, line);
+			return line;
+		}
+	}
+
+	return line;
+}
+
+string getComplietedMaps(string* playerMapsPointer) {
+
+	string mapName;
+
+	cout << "List of completed maps:" << endl;
+	printListOfMaps(playerMapsPointer);
+
+	cout << "Type the number of the map you want to play!" << endl;
+	int mapNumber;
+	cin >> mapNumber;
+
+	mapName = getMapName(mapNumber, playerMapsPointer);
+
+	while (mapName == "WRONG_NUMBER_INPUT") {
+
+		cout << "Number \"" << mapNumber << "\" is invalid!" << endl;
+		mapName = getMapName(mapNumber, playerMapsPointer);
+	}
+
+	return mapName;
+}
+
+string levelSelector(string playerName) {
+
+	string* playerMapsPointer = loadPlayerMaps(playerName);
+
+	if (*playerMapsPointer == "") {
+
+		return "EMPTY_LIST";
+	}
+
+	string command;
+
+	cout << "Would you like to continue or play a previous map?" << endl;;
+	cout << "Continue/Select" << endl;;
+	cin >> command;
+	command = toUpper(command);
+
+	while (!levelSelectorCommandValidator(command)) {
+
+		cout << "Command \"" << command << "\" is invalid!" << endl;
+		cin >> command;
+		command = toUpper(command);
+	}
+
+	string mapName;
+
+	if ((command == "CONTINUE")) {
+
+		mapName = getLastPlayedPlayerMap(playerName);
+	}
+	else {
+
+		mapName = getComplietedMaps(playerMapsPointer);
+	}
+
+	return mapName;
+}
+
+int main()
+{
+
+	string playerName = startPlayerSystem();
+	string mapName = levelSelector(playerName);
 
 	const int row = 20;
 	const int col = 20;
